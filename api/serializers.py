ba3e811a84b436c42dccc33144a53b19ce9827a0
rajesh <rajeshniure567@gmail.com>
2025-05-28 02:17:21 +0545

    Nested Serializer, Pagination and Filtering fuctionality

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = "__all__"
+        
+        
+        
--- api/views.py
+++ api/views.py
@@ -9,6 +9,10 @@ from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
 from rest_framework import mixins, generics,viewsets
+from blogs.models import Blog, Comment
+from blogs.serilaizers import BlogSerializer, CommentSerializer
+from .paginations import CustomPagination
+from employees.filters import EmployeeFilter
 # Create your views here.
 
 
@@ -196,3 +200,31 @@ class EmployeeViewset(viewsets.ViewSet):
 class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    pagination_class = CustomPagination
+    # filterset_fields = ['designation']
+    filterset_class = EmployeeFilter
+    
+    
+    
+    
+class BlogsView(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    
+    
+class CommentsView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+
+
+class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    lookup_field = 'pk'
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_field = 'pk'
+    
